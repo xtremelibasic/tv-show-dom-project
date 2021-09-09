2b79dab314c373236644dfc92723e33196deb031
Amanda Obiajulu Nwadukwe <xtremeli.basic@gmail.com>
2021-09-09 23:50:54 +0100

    level 100 tasks completed

--- script.js
+++ script.js
@@ -6,7 +6,49 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.className = "episodes";
+
+  episodeList.forEach(episode => {
+    
+    //DIV
+    const episodeDiv = document.createElement("div");
+    episodeDiv.className = "episode";
+    rootElem.append(episodeDiv);
+
+    //Episode Title
+    const title = document.createElement("h2");
+    let seasonNumber = String(episode.season);
+    let episodeNumber = String(episode.number);
+    if(seasonNumber.length > 1){
+      seasonNumber = seasonNumber;
+    } else {
+      seasonNumber = "0" + seasonNumber;
+    }
+    if(episodeNumber.length > 1){
+      episodeNumber = episodeNumber;
+    } else {
+      episodeNumber = "0" + episodeNumber;
+    }
+    title.innerText = `${episode.name} S${seasonNumber}E${episodeNumber}`;
+    episodeDiv.append(title)
+    
+    //Thumbnail
+    const thumbnail = document.createElement("img");
+    thumbnail.className = "thumbnail";
+    thumbnail.src = episode.image.medium;
+    thumbnail.alt = `${episode.name} season${episode.season} episode${episode.number}`;
+    episodeDiv.append(thumbnail)
+
+    //Summary
+    const summary = document.createElement("div");
+    summary.innerHTML = episode.summary;
+    episodeDiv.append(summary);
+  })
+
+  //Disclaimer
+  const disclaimer = document.createElement("h4");
+  disclaimer.innerHTML = "All data in this page was originally gotten from <a href='https://www.tvmaze.com/1'>TVMaze.com</a>"
+  rootElem.append(disclaimer);
 }
 
 window.onload = setup;
